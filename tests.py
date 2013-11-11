f153d15bd3c3bdd2bb156e1b986936dbdcd74970
Aaron Cossey <aaron.cossey@gmail.com>
2013-11-11 20:18:46 +0100

    add tests for BoardTile

--- tests.py
+++ tests.py
@@ -22,9 +22,43 @@ class TestTile(unittest.TestCase):
         
     def test_attributes(self):
         self.assertIs(type(BoardTile(self.sample_exits1).id), int)
+        self.assertIs(type(BoardTile(self.sample_exits1,'').item), str)
+        self.assertIs(type(BoardTile(self.sample_exits1,'','').action), str)
+        self.assertEqual(self.sample_exits1, BoardTile(self.sample_exits1).exits)
+        
+    def test_rotate(self):
+        sample1_rotations = [ [False,True,True,False],  #90
+                              [False,False,True,True],  #180
+                              [True,False,False,True],  #270
+                              [True,True,False,False] ] #360
+        
+        test_tile = BoardTile(self.sample_exits1)
+        for rotation in sample1_rotations:
+            test_tile.rotate()
+            self.assertEqual(rotation, test_tile.exits)
+        
+    def test_randomise_orientation(self):
+        pass
+        rotation_counter = { str([False,True,True,False]):0.0,
+                             str([False,False,True,True]):0.0,
+                             str([True,False,False,True]):0.0,
+                             str([True,True,False,False]):0.0 }
+        test_tile = BoardTile(self.sample_exits1)
+        num_trials = 100000
+        for i in xrange(0,num_trials):
+            test_tile.randomise_orientation()
+            rotation_counter[str(test_tile.exits)] += 1
+        counts = rotation_counter.values()
+        mean = sum(counts)/len(counts)
+        std_deviation = (sum([j**2 for j in [i - mean for i in counts]])/len(counts))**0.5
+        p = std_deviation/num_trials
+        self.assertTrue(p<0.05, 'p value was ' + str(p))
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestPlayer)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestTile)
 unittest.TextTestRunner(verbosity=2).run(suite)
+
+
